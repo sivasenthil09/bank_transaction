f3c27d7b45a36c2b44c09b25ccdff25965f78eed
sivasenthil09 <109544964+sivasenthil09@users.noreply.github.com>
2023-08-31 20:58:18 +0530

    Delete bank_transaction project directory

--- bank_transaction project/interfaces/index.go
+++
@@ -1,8 +0,0 @@
-package interfaces
-
-import "github.com/sivasenthil09/bank_transaction/models"
-
-type ICustomer interface {
-	CreateCustomer(customer *models.Customer) (*models.DBResponse, error)
-	Transaction(name string)(*models.DBResponse,error)
-}
